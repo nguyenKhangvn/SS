65ba91a63112f7ba1058df4a7b330231d69867d7
Nguyen Dinh Khang <dinhkhang417sc@gmail.com>
2025-04-28 21:05:34 +0700

    login w gg

--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:5173") 
               .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowCredentials(); // Allow cookies and credentials
+              .AllowCredentials();
     });
 });
 
@@ -36,6 +36,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
 }
 app.UseHttpsRedirection();
+//au 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapProductApi();
 app.MapCategoryApi();
 app.MapStoreLocationApi();
